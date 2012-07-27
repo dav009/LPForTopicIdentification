df248fe6edc24e14731e1f2fc605ce13e34e27cb
David Przybilla <dav.alejandro@gmail.com>
2012-07-27 18:26:33 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#David Przybilla
+#this file is the main of the app
+
+
+def main:
+	#call the feature extration
+
+	#measure the similarities
+
+	#define the graph
+
+	#call JUNTO
+
+	#asses output
+
+
+main()
+
