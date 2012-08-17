f8325ac949eb0e901cd6b7ab3ebf904fca16979e
David Przybilla <dav.alejandro@gmail.com>
2012-08-17 19:42:32 +0200

    Added new function for calculating PMI

--- IO/data.py
+++ IO/data.py
@@ -35,6 +35,50 @@ class Instance:
 		#apply some similarity measure (cosine)
 		return 1
 
-	
+
+#Auxialiry Functions
+
+#get sets of Words per Label
+#given a list of Instance bojects it returns per each label,the list of words common to that label with high frequency
+#returns a list of Set
+#input: setOfWords : the vocabulary
+#		listOfinstances: list of messages
+# 		type: type of grouping which want to be achieved
+#processing: iterate for every word in the vocabulary looking its predictve structure with respect to each label
+#output: list of of [word,label,pmi]      
+def getSetOfWordsPerLabel(setOfLabels,setOfWords,listOfInstances,type):
+
+	returnList=[]
+	if(type=="highFrequency"):
+		#get words with highfreqenquecny per label
+		print "getting words with high freqeuncy group by label"
+	if(type=="PMI"):
+		#get words that are the best predictors for a label 
+
+		print "getting words with high freqeuncy group by PMI"
+
+		for(label in setOfLabels):
+			
+			for(word in setOfLabels):
+				triple={}
+				#how many times word-label have been seen together
+				labelWordCount=0.0
+				#how many times a word has been seen in the instances
+				wordCount=0.0
+				#how many times the label has been seen in the instances
+				labelCount=0.0
+				for(instance in listOfInstances):
+					if(instance.getFrecuencyTable.get(word)>0 && instance.triple['label']==label):
+						labelWordCount=labelWordCount+1.0
+					if(instance.triple['label']==label):
+						labelCount=labelCount+1.0
+					if(instance.getFrecuencyTable.get(word)>0):
+						wordCount=wordCount+1.0
+				triple['word']=word
+				triple['label']=label
+				triple['pmi']=math.log(labelWordCount/(wordCount*labelCount))
+			returnList.append(triple)
+
+
 		
 		
